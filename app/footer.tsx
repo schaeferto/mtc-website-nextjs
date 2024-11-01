2fe13c4bdc1f208ed38264bdc3ee06d0fc32765d
schaeferto <tobias.schaefer.12@googlemail.com>
2024-11-01 22:10:44 +0100

    Footer image

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import footerImage from "../public/footer-test.png";
+
 export default function Footer() {
   return (
     <footer className={`bg-mtc-black`}>
+      <div className={"bg-mtc-black w-full h-28"}></div>
+      <div className={"flex w-full h-[240px] bg-mtc-black"}>
+        <Image
+          src={footerImage}
+          alt={"footer"}
+          className={"w-full h-full object-cover object-top bg-mtc-black"}
+        ></Image>
+      </div>
       <div className="flex flex-col justify-center items-center text-sm gap-4 py-6">
         <span>Impressum</span>
         <div>© 2024 Munich Triathlon Club e.V.</div>
