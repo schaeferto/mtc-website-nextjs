d1e07c1b213e8bd153fb9b79d1d6e18d39ade105
schaeferto <tobias.schaefer.12@googlemail.com>
2024-11-02 21:27:18 +0100

    Add new home

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,45 @@
+import coverImage from "../public/cover_2.webp";
+import Image from "next/image";
+import "./globals.css";
+
+const Overlay = () => {
+  return (
+    <div className={"absolute h-full w-full flex flex-col items-center"}>
+      <div className={"grow flex flex-col items-center"}>
+        <div
+          className={`text-[80px]  md:text-[140px] mt-12 text-center text-white font-findel`}
+        >
+          M T C
+        </div>
+        <div
+          className={`text-[30px] md:text-[60px] md:mt-0 mt-12 text-center text-white font-montserrat`}
+        >
+          Munich Triathlon Club
+        </div>
+        <div
+          className={`text-xl mt-16 md:w-full w-2/3 text-center text-white font-montserrat`}
+        >
+          Herzlich Willkommen im Biber-Club!
+        </div>
+      </div>
+      <button
+        className={"bg-mtc-yellow py-3 px-8 mb-20 rounded-full text-black self"}
+      >
+        JETZT MITMACHEN
+      </button>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <div className={`flex h-screen w-full relative`}>
-      <h1
-        className={`absolute inset-0 flex items-center justify-center text-4xl text-center text-white font-bold font-findel`}
-      >
-        MTC
-      </h1>
-      <h2
-        className={`top-48 absolute inset-0 flex items-center justify-center text-2xl text-center text-white font-bold font-montserrat`}
-      >
-        Soon in next.js ...
-      </h2>
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
+    <div className={`flex h- w-full relative display-height`}>
+      <Overlay />
+      <Image
         className={`h-full w-full object-cover`}
-      >
-        <source src="/header-video.webm" type="video/webm" />
-        <source src="/header-video.mp4" type="video/mp4" />
-      </video>
+        src={coverImage}
+        alt={"Home Background"}
+      ></Image>
     </div>
   );
 }
