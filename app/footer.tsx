4eef84a739addc233ee5fadc110a1f43a306d0bc
schaeferto <tobias.schaefer.12@googlemail.com>
2024-11-06 23:26:28 +0100

    Continue work on HOME

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import footerImage from "../public/free-footer.webp";
+import { PiGithubLogoFill } from "react-icons/pi";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className={`bg-mtc-black`}>
-      <div className={"bg-mtc-black w-full h-[500px]"}></div>
+    <footer className={`bg-mtc-black pt-[80px]`}>
       <div className={"flex w-full bg-mtc-black"} style={{ height: "55vw" }}>
         <Image
           src={footerImage}
@@ -15,16 +16,21 @@ export default function Footer() {
         ></Image>
       </div>
       <div className="flex flex-col justify-center items-center text-sm gap-4 py-6">
-        <span>Impressum</span>
+        <Link href={"/impressum"}>Impressum</Link>
         <div>© 2024 Munich Triathlon Club e.V.</div>
         <div className="flex flex-col items-center justify-center">
           <div>Made with ❤️ by</div>
-          <div>
-            <i className="fa-brands fa-github pr-2 text-zinc-500"></i>
-            <a href="https://github.com/schaeferto" target="_blank">
-              schaeferto
-            </a>
-          </div>
+          <a
+            href="https://github.com/schaeferto"
+            target="_blank"
+            className={""}
+          >
+            <PiGithubLogoFill
+              className={"inline mr-1"}
+              size={14}
+            ></PiGithubLogoFill>
+            <span>schaeferto</span>
+          </a>
         </div>
       </div>
     </footer>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import coverImage from "../public/cover_2.webp";
-import Image from "next/image";
+import mottoImage from "../public/motto.jpg";
+import homeLeagueImage from "../public/home_liga.jpg";
+import homeContactImage from "../public/home_contact.jpg";
+import homeTrainingImage from "../public/home_training.jpg";
+import Image, { StaticImageData } from "next/image";
 import "./globals.css";
+import Link from "next/link";
 
 const Overlay = () => {
   return (
@@ -23,7 +28,7 @@ const Overlay = () => {
         </div>
       </div>
       <div className={"mb-40 mt-12"}>
-        <button className={"bg-mtc-yellow py-3 px-8 rounded-full text-black"}>
+        <button className={"bg-mtc-yellow py-3 px-8 rounded-full text-black "}>
           JETZT MITMACHEN
         </button>
       </div>
@@ -31,15 +36,128 @@ const Overlay = () => {
   );
 };
 
+export function CoverImage() {
+  return (
+    <Image
+      className={`h-full w-full object-cover`}
+      src={coverImage}
+      alt={"Home Background"}
+    />
+  );
+}
+
+export function Cover() {
+  return (
+    <div className={`flex w-full relative first-content-height`}>
+      <Overlay />
+      <CoverImage />
+    </div>
+  );
+}
+
+export function Motto() {
+  return (
+    <div
+      className={
+        "bg-mtc-background text-mtc-black flex flex-col items-center p-8"
+      }
+    >
+      <h2 className={"text-2xl md:text-4xl font-bold my-8"}>Unser Motto</h2>
+      <Image
+        src={mottoImage}
+        alt={"The Team cheering."}
+        className="w-[300px] md:w-[600px]"
+      ></Image>
+      <div className={"font-bold mt-8"}>#biberfieber</div>
+      <p className={"text-center"}>
+        Willkommen beim MTC München, dem Munich Triathlon Club. Wir sind ein
+        junger, dynamischer Verein, der es sich zur Aufgabe gemacht hat, durch
+        Zusammenhalt, Freundschaft, Transparenz und Sportsgeist das Vereinsleben
+        gemeinsam zu gestalten. Wir möchten gemeinsam wachsen, sportliche
+        Erfahrungen sammeln und alle, die es möchten, mit unserem #biberfieber
+        anstecken.
+      </p>
+      <p className={"mt-8 text-center"}>
+        Warum Biber? Warum denn nicht, oder habt ihr noch nie etwas von den
+        Bibern an der Isar gehört? 😉
+      </p>
+    </div>
+  );
+}
+
+function ImageWithTextAndLink({
+  src,
+  alt,
+  text,
+  href,
+}: {
+  src: StaticImageData;
+  alt: string;
+  text: string;
+  href: string;
+}) {
+  return (
+    <Link href={href} className={"relative w-full"}>
+      <Image src={src} alt={alt}></Image>
+      <div className={"absolute inset-0 w-full h-full flex"}>
+        <div
+          className={
+            "backdrop-blur-sm m-auto px-3 md:px-8 py-1 md:py-2 bg-mtc-black bg-opacity-60 md:text-3xl font-bold"
+          }
+        >
+          {text}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+function SectionLinks() {
+  return (
+    <div
+      className={
+        "flex w-full items-stretch bg-mtc-background p-2 md:p-12 gap-2 md:gap-6 pb-8"
+      }
+    >
+      <ImageWithTextAndLink
+        href={"/league"}
+        src={homeLeagueImage}
+        alt={"Liga"}
+        text={"Liga"}
+      />
+      <ImageWithTextAndLink
+        href={"/training"}
+        src={homeTrainingImage}
+        alt={"Training"}
+        text={"Training"}
+      />
+      <ImageWithTextAndLink
+        href={"/contact"}
+        src={homeContactImage}
+        alt={"Kontakt"}
+        text={"Kontakt"}
+      />
+    </div>
+  );
+}
+
+function News() {
+  return (
+    <div className={"bg-mtc-background text-mtc-black"}>
+      <div className={"text-xl font-bold text-center pb-8"}>UNSERE NEWS</div>
+      <hr className={"border-mtc-black mx-4"} />
+      <div className={"h-12"}></div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
-    <div className={`flex h- w-full relative display-height`}>
-      <Overlay />
-      <Image
-        className={`h-full w-full object-cover`}
-        src={coverImage}
-        alt={"Home Background"}
-      ></Image>
-    </div>
+    <>
+      <Cover />
+      <Motto />
+      <SectionLinks />
+      <News />
+    </>
   );
 }
