e7910d7b50caef81c0a8b6e0edc763843b8e80ca
schaeferto <tobias.schaefer.12@googlemail.com>
2024-12-21 00:17:55 +0100

    Add training page

---
+++ app/news/news.ts
@@ -0,0 +1,21 @@
+import newsAllgaeuImage from "@/public/allgaeu_news.jpg";
+import newsSchongauImage from "@/public/schongau_news.jpg";
+import newsChallengeImage from "@/public/challenge_news.jpg";
+
+export const newsShort = [
+  {
+    image: newsAllgaeuImage,
+    header: "Allgäu Triathlon 2024",
+    text: "Es war ein Spektakel. Der Allgäu Triathlon 2024. Unsere Biber waren auf der Mitteldistanz und der Olympischen Distanz unterwegs. Was alle gemeinsam hatten: Sie wurden plörre nass. Inklusive der Biber-Fans am Rand. Da half die beste Regenjacke nicht. Am Ende schafften es drei Biber sogar aufs Podium!",
+  },
+  {
+    image: newsSchongauImage,
+    header: "Landesliga Süd Damen Schongau",
+    text: "Unsere Biber-Damen haben die Ligasaison eröffnet und am vergangenen Wochenende haben sie sie auch in Schongau beendet. Es war uns ein Fest! Bei strahlendem Sonnenschein und unter stürmischem Biber-Jubel gingen Janet, Lisa, Vicky und Anne an den Start. Die dritte Person im Ziel wurde gewertet, also war eine Team-Strategie gefragt. Wie so oft, musste die flexibel noch einmal angepasst werden. Ins Ziel gekommen sind sie aber alle!",
+  },
+  {
+    image: newsChallengeImage,
+    header: " Challenge Roth 2024",
+    text: "Am ersten Juli-Wochenende stand für einige unserer Biber das Saisonhighlight an. Bei der Challenge Roth gingen Valentin, Jens und Anne an den Start. Valentin schaffte seine Bestzeit auf der Langdistanz und Jens feierte in Roth sein Langdistanz-Debüt. Wo ginge das besser, als bei dem weltweit größten Wettkampf auf der Triathlon-Langdistanz.",
+  },
+];
