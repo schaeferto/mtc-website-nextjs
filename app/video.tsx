17fbb2d08720d732b05217a11806084ee510da50
schaeferto <tobias.schaefer.12@googlemail.com>
2025-09-07 00:10:01 +0200

    Enhance video component styling for full-width display

--- app/video.tsx
+++ app/video.tsx
@@ -1,12 +1,12 @@
 export async function Video({ fileName }: { fileName: string }) {
   return (
-    <div className={"min-h-[inherit]"}>
+    <div className={"min-h-[inherit] w-full"}>
       <video
         autoPlay
         muted
         loop
         playsInline
-        className={"min-h-[inherit] object-cover"}
+        className={"min-h-[inherit] w-full h-full object-cover"}
       >
         <source src={"/" + fileName + ".webm"} type="video/webm" />
       </video>
