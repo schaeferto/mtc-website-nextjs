d91a3fa7e393eda3fc1e58fd7f00a03e001809d9
schaeferto <tobias.schaefer.12@googlemail.com>
2025-09-17 22:04:32 +0200

    fix: Text on carousel and autoplay

--- app/image-with-textoverlay.tsx
+++ app/image-with-textoverlay.tsx
@@ -13,21 +13,15 @@ export function ImageWithTextAndLink({
   href: string;
 }) {
   return (
-      <Link href={href} className={"w-full h-full block"}>
-        <Image src={src} alt={alt}></Image>
-        <div
-          className={
-            "absolute flex flex-col items-center justify-center w-full h-full top-0 left-0"
-          }
-        >
-          <div
-            className={
-              "backdrop-blur-sm px-3 md:px-8 py-1 md:py-2 bg-mtc-black bg-opacity-60 md:text-4xl text-2xl font-bold text-white"
-            }
-          >
+    <div className="relative w-full h-full">
+      <Link href={href} className="block w-full h-full">
+        <Image src={src} alt={alt} className="w-full h-full object-cover" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center">
+          <div className="backdrop-blur-sm px-3 md:px-8 py-1 md:py-2 bg-mtc-black bg-opacity-60 md:text-4xl text-2xl font-bold text-white rounded-sm">
             {text}
           </div>
         </div>
       </Link>
+    </div>
   );
 }
