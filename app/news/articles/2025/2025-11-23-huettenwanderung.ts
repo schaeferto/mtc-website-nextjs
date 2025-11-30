47265bb508348b7c352ef5a16190173d5f452931
schaeferto <tobias.schaefer.12@googlemail.com>
2025-11-30 20:44:27 +0100

    feat: add news article

---
+++ app/news/articles/2025/2025-11-23-huettenwanderung.ts
@@ -0,0 +1,15 @@
+import huettenwanderungImage from "@/public/2025-11-23_news_huettenwanderung.jpg";
+import { NewsArticle } from "../../types";
+
+export const huettenwanderung: NewsArticle = {
+  id: "huettenwanderung-2025-11-23",
+  slug: "huettenwanderung-2025-11-23",
+  image: { src: huettenwanderungImage, height: 1200, width: 1600 },
+  header: "Hüttenwanderung im Schnee",
+  text: [
+    "Off-Season heißt Beine hochlegen und nichts tun? Weit verfehlt. Biber machen keinen Winterschlaf! 😏",
+    "Vollgepackt mit gutem Essen ging es vor einer Woche zusammen hoch auf die Probstalm. Für viele Biber der erste Schnee der Saison. ❄ Ca. 3 Stunden Aufstieg - 12 KM und 700 Höhenmeter. 🏔 Angemessen zackiges Tempo - wir sind ja TriathletInnen... Ein tolles Wochenende, mit einer mal etwas anderen Aktivität. 🤗",
+  ],
+  date: "2025-11-23",
+  releaseDate: "2025-11-30",
+};
