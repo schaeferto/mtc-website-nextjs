12e5c3ef15133b509d01aa3e00b0ae87a7fa263a
Tobias Schäfer <tobias.schaefer2@cgm.com>
2026-05-29 12:05:44 +0000

    chore: update build script to include payload migration and add push configuration to payload settings

--- next-js-frontend/payload.config.ts
+++ next-js-frontend/payload.config.ts
@@ -32,6 +32,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || "",
     },
+    push: process.env.PAYLOAD_DB_PUSH === 'true',
   }),
   sharp,
   plugins: [
